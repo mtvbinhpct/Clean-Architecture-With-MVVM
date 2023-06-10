690686b5ec8868e367c0fedbe0f02ed221c92f1a
binhpct <binhpct@techvn.mti.co.jp>
2023-06-10 15:14:00 +0700

    add base

---
+++ lib/presentation/resources/font_manager.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class FontConstants {
+  static const String fontFamily = 'Montserrat';
+}
+
+class FontWeightManager {
+  static const FontWeight light = FontWeight.w300;
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight medium = FontWeight.w500;
+  static const FontWeight semiBold = FontWeight.w600;
+  static const FontWeight bold = FontWeight.w700;
+}
+
+class FontSize {
+  static const double s12 = 12.0;
+  static const double s14 = 12.0;
+  static const double s16 = 12.0;
+  static const double s17 = 12.0;
+  static const double s18 = 12.0;
+  static const double s20 = 12.0;
+}
